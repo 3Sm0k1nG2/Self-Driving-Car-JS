4ff3d2949278a4ba8f0ed53f1e8cc21f172037f6
3Sm0k1nG2 <37252090+3Sm0k1nG2@users.noreply.github.com>
2024-01-12 08:45:22 +0200

    Complete 'Segment intersection formula explained' tutorial

--- js/utils.js
+++ js/utils.js
@@ -18,24 +18,35 @@ export function lerp(A, B, t) {
  * @param {Point} B 
  * @param {Point} C 
  * @param {Point} D 
- * @returns 
+ *   
+ * Linear Equation  
+ * | Ix = Ax+(Bx-Ax)*t = Cx+(Dx-Cx)*u  
+ * | Iy = Ay+(By-Ay)*t = Cy+(Dy-Cy)*u
  */
-export function getIntersection(A,B,C,D){ 
-    const tTop=(D.x-C.x)*(A.y-C.y)-(D.y-C.y)*(A.x-C.x);
-    const uTop=(C.y-A.y)*(A.x-B.x)-(C.x-A.x)*(A.y-B.y);
-    const bottom=(D.y-C.y)*(B.x-A.x)-(D.x-C.x)*(B.y-A.y);
-    
-    if(bottom!=0){
-        const t=tTop/bottom;
-        const u=uTop/bottom;
-        if(t>=0 && t<=1 && u>=0 && u<=1){
-            return new Intersection(
-                lerp(A.x,B.x,t),
-                lerp(A.y,B.y,t),
-                t
-            );
-        }
+export function getIntersection(A, B, C, D) {
+    const tDividend = (D.x-C.x)*(A.y-C.y) - (D.y-C.y)*(A.x-C.x);
+    const uDividend = (C.y-A.y)*(A.x-B.x) - (C.x-A.x)*(A.y-B.y);
+    const commonDivisor = (D.y-C.y)*(B.x-A.x) - (D.x-C.x)*(B.y-A.y);
+
+    if(commonDivisor === 0) {
+        return null;
     }
 
-    return null;
+    const t = tDividend / commonDivisor;
+    const u = uDividend / commonDivisor;
+
+    if(
+        t < 0 
+        || t > 1 
+        || u < 0 
+        || u > 1
+    ){
+        return null;
+    }
+
+    return new Intersection(
+        lerp(A.x, B.x, t),
+        lerp(A.y, B.y, t),
+        t
+    );
 }
